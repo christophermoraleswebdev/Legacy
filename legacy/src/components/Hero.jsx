0d947f5cda461e142cf901bf8772babc88722fd1
Christopher Morales <christopher.moraleswebdev@gmail.com>
2023-08-10 13:08:12 -0500

    Created navbar and hero section

---
+++ legacy/src/components/Hero.jsx
@@ -0,0 +1,17 @@
+
+
+const Hero = () => {
+  return (
+    <div className="hero-container">
+      <h1>Empowering Excellence: Your Partner in Claims Solutions</h1>
+      <h3>Delivering Customer Satisfaction & Precision Assessments</h3>
+      <p>At Legacy Independent Services, we're dedicated to delivering top-notch solutions that ensure customer satisfaction and accuracy. Our expertise spans a wide range of services, including claims analysis, comprehensive written estimates, meticulous roof assessments, ladder assist support, claims processing, appraisals, and streamlined administrative claims tasks. With a proven track record and a commitment to excellence, we're your trusted partner for all your claims-related needs</p>
+      <div className="button-container">
+            <button className="hero-btn">Our Services</button>
+            <button className="hero-btn">Contact Us</button>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
---
+++ legacy/src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+
+const Navbar = () => {
+  return (
+    <div className="navbar-container">
+      <div className="logo-container">
+            <img src="https://i.ibb.co/ch3V51s/Legacy-Logo.png" />
+            <h4>egacy Independent Services</h4>
+      </div>
+
+      <div className="menu">
+            <p>Services</p>
+            <p>Contact Us</p>
+            <p>About Us</p>
+      </div>
+
+      <div className="hamburger-menu-container">
+            <p>Services</p>
+            <p>Contact Us</p>
+            <p>About Us</p>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
