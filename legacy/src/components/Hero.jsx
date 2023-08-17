d9fb99cf393628d6394fe48cbf25e835e83745b5
Christopher Morales <christopher.moraleswebdev@gmail.com>
2023-08-17 10:52:43 -0500

    added functionality to the contact us and our services btn

--- legacy/src/components/Hero.jsx
+++ legacy/src/components/Hero.jsx
@@ -7,11 +7,11 @@ const Hero = () => {
       <h3>Delivering Customer Satisfaction & Precision Assessments</h3>
       <p>At Legacy Independent Services, we are dedicated to delivering top-notch solutions that ensure customer satisfaction and accuracy. Our expertise spans a wide range of services, including claims analysis, comprehensive written estimates, meticulous roof assessments, ladder assist support, claims processing, appraisals, and streamlined administrative claims tasks. With a proven track record and a commitment to excellence, we're your trusted partner for all your claims-related needs</p>
       <div className="button-container">
-            <button className="hero-btn">Our Services</button>
-            <button className="hero-btn">Contact Us</button>
+        <button className="hero-btn"><a href="#services">Our Services</a></button>
+        <button className="hero-btn"><a href="tel:+19562996674">Contact Us</a></button>
       </div>
     </div>
   )
 }
 
-export default Hero
+export default Hero;
