cd8ac48544b2d9d49ed928bef3aeeb02939ab77f
Christopher Morales <christopher.moraleswebdev@gmail.com>
2023-08-17 12:42:40 -0500

    updated hero section

--- legacy/src/components/Hero.jsx
+++ legacy/src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
   return (
     <div className="hero-container">
       <h1>Empowering Excellence: Your Partner in Claims Solutions</h1>
-      <h3>Delivering Customer Satisfaction & Precision Assessments</h3>
+      <h2>Legacy Helps With Claims</h2>
       <p>At Legacy Independent Services, we are dedicated to delivering top-notch solutions that ensure customer satisfaction and accuracy. Our expertise spans a wide range of services, including claims analysis, comprehensive written estimates, meticulous roof assessments, ladder assist support, claims processing, appraisals, and streamlined administrative claims tasks. With a proven track record and a commitment to excellence, we're your trusted partner for all your claims-related needs</p>
       <div className="button-container">
         <button className="hero-btn"><a href="#services">Our Services</a></button>
